5c063b8de307b0cd4dfaeb651dc3844720fafaa9
Shania Cabrera <136392662+shania-cabrera@users.noreply.github.com>
2024-03-15 21:47:29 +0000

    Initiliazing repo: for Shania

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,9 @@ function App() {
       <section className="h-screen w-[80%] flex mx-auto items-center">
         <div className="h-auto flex flex-row sm:flex-col-reverse space-x-10 sm:space-x-0 ">
           <div className="flex flex-col justify-center space-y-5 text-base">
-            {/* TODO 1: Edit the content of these tags!*/}
-            <h1>Hi! I'm ______</h1>
+            <h1>Hi! I'm Shania Cabrera</h1>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Vestibulum ac ante eget arcu ultrices eleifend. Curabitur
-              tincidunt, quam non consequat cursus, nibh velit ultricies lorem,
-              et accumsan augue justo a diam.
+             I study information science and game design at Cornell University.
             </p>
             <div className="flex flex-row items-center space-x-2">
               <img src={location} />
@@ -32,24 +28,22 @@ function App() {
               <p>Avaliable for new projects</p>
             </div>
 
-            {/* TODO 2: Change the links of these tags to link to your socials! */}
             <div className="flex flex-row h-10 space-x-2">
-              <a href="">
+              <a href="https://github.com/shania-cabrera?tab=repositories">
                 <img src={githublogo} />
               </a>
               <a href="">
                 <img src={instalogo} />
               </a>
-              <a href="">
+              <a href="https://www.figma.com/files/recents-and-sharing/recently-viewed?fuid=1166832836082532104">
                 <img src={figmalogo} />
               </a>
-              <a href="">
+              <a href="https://www.linkedin.com/in/shania-cabrera/">
                 <img src={linkedinlogo} />
               </a>
             </div>
           </div>
           <div className="flex justify-center ">
-            {/* TODO 3: Change this to a custom image! */}
             <img
               src={wicclogo}
               alt="WICC Logo"
